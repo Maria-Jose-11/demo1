20d4ef09883af31041d1adeb2bd333cbeaae8e9b
Maria-Jose-11 <mariajochala@gmail.com>
2022-02-16 14:17:12 -0500

    Initial commit

---
+++ src/main/java/com/example/demo1/Controlador.java
@@ -0,0 +1,40 @@
+package com.example.demo1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class Controlador {
+
+
+    @FXML
+    private AnchorPane anchopanel;
+
+
+    @FXML
+    private Button boton1;
+
+    @FXML
+    private Button boton2;
+
+    @FXML
+    private Label labelnombre;
+
+    @FXML
+    private Label labeltitle;
+
+    @FXML
+    void cambiar1(ActionEvent event) {
+
+        anchopanel.getStylesheets().add(getClass().getResource("/estilo1.css").toExternalForm());
+    }
+
+    @FXML
+    void cambiar2(ActionEvent event) {
+        anchopanel.getStylesheets().add(getClass().getResource("/estilo2.css").toExternalForm());
+
+    }
+
+}
